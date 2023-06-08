6f3924dcad22ed889c0f42ea10bccea8cfb874a4
Mitch Rickman <mitch.rickman@intellitect.com>
2023-06-08 15:52:59 -0700

    Add blazor forms sample

---
+++ BlazorForms/Final/BlazorForms/Data/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorForms.Data
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Please provide a first name.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+
+        private string? _Email { get; set; }
+
+        [EmailAddress]
+        [AllowNull]
+        public string? Email
+        {
+            get
+            {
+                return this._Email;
+            }
+            set
+            {
+                this._Email = string.IsNullOrWhiteSpace(value) ? null : value;
+            }
+        }
+    }
+}
---
+++ BlazorForms/Starting/BlazorForms/Data/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorForms.Data
+{
+    public class Contact
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+    }
+}
